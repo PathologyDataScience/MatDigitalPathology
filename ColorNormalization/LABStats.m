cb6f0f42a2243cda49b5878712fa5e62e7f1dded
Lee Cooper <lee.cooper@emory.edu>
2017-02-21 10:40:46 -0500

    Create LABStats.m

---
+++ ColorNormalization/LABStats.m
@@ -0,0 +1,35 @@
+function [Mu, Sigma, LAB] = LABStats(I, Mask)
+%Extracts first and second moment statistics from input RGB image 'I' in 
+%the LAB color space. Statistics are calculated in a masked region
+%containing tissue and not glass/background.
+
+%inputs:
+%I - (M x N x 3 uint8) RGB color image.
+%Mask - (M x N logical) Mask of pixels to use in calculating normalization
+%       parameters. Optional. Can be provided as an alternative to
+%       performing the linear discriminant analysis.
+
+%outputs:
+%Mu - (3-length float) Mean values of target color normalization image in 
+%     LAB color space.
+%Sigma - (3-length float) Mean values of target color normalization image
+%        in LAB color space.
+%LAB - (M x N x 3 float) LAB color image result.
+
+%Get image size to simplify reshaping
+M = size(I, 1);
+N = size(I, 2);
+
+%transform image to LAB space
+LAB = RGB2LAB(I);
+
+%transform to vectorized format
+vLAB = reshape(LAB, [M*N 3]).';
+
+%mask and remove NaN and Inf entries
+vLAB(:, sum(isnan(vLAB), 1) > 0 | sum(isinf(vLAB), 1) | ...
+        ~reshape(Mask, [M*N 1]).') = [];
+
+%Get LAB statistics of input image
+Mu = mean(vLAB, 2);
+Sigma = std(vLAB, [], 2);
